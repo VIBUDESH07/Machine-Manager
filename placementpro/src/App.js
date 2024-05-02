204aa07b5a194156081ed719927b9a31e1b7df6f
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-05-02 11:26:22 +0530

    Initialize

---
+++ placementpro/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header'; // Assuming Header and Footer are components in separate files
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
