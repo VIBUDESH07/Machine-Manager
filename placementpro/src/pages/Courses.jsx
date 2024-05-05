057a0c16741d5e67488ca72c60d2726286545a18
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-05-05 19:51:33 +0530

    update

--- placementpro/src/pages/Courses.jsx
+++ placementpro/src/pages/Courses.jsx
@@ -3,34 +3,40 @@ import Stusidebar from '../components/Stusidebar';
 import axios from 'axios';
 
 const Courses = () => {
-    // State to store courses data
+    const [studentDetails, setStudentDetails] = useState(null);
 
     useEffect(() => {
         const fetchStudentData = async () => {
             try {
-                const studentId = JSON.parse(localStorage.getItem('studentDetails')).student_id;
-                const response = await axios.get(`http://localhost:5000/student/${studentId}`); 
-               
+                const student = JSON.parse(localStorage.getItem('studentDetails'));
+                const response = await axios.get(`http://localhost:5000/student/${student.student_id}`);
+                setStudentDetails(response.data); // Set student details with the response data
             } catch (error) {
                 console.error('Error fetching student data:', error);
             }
         };
 
-        // Call the fetchStudentData function when the component mounts
         fetchStudentData();
-    }, []); // Empty dependency array to ensure the effect runs only once
+    }, []);
 
     return (
-        <div>
-           
-            <div style={{ display: 'flex', alignItems: 'center', paddingLeft: '640px',paddingTop:'20px'}}>
-                {/* Render the fetched courses data */}
-                <h2>Courses</h2>
-                <ul>
-                   
-                </ul>
+        <div style={{ display: 'grid', gridTemplateColumns: '1fr 3fr' }}>
+            {/* Left column */}
+            <div style={{ backgroundColor: 'lightblue',height:'200px' ,padding: '20px' }}>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <h2>Courses</h2>
+                </div>
+                
             </div>
-            <Stusidebar />
+            {/* Right column */}
+            <div style={{ backgroundColor: 'greenyellow', padding: '20px' }}>
+                {/* Make sure studentDetails is not null before accessing its properties */}
+                {studentDetails && <h2>Hello Mr. {studentDetails.name}</h2>}
+            </div>
+            <div>
+                Hello
+            </div>
+            
         </div>
     );
 };
