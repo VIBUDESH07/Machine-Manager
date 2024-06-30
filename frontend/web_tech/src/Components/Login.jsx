c07da227dc1ecd09d06677e575ed3ceed22d6f64
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-06-30 13:52:42 +0530

    Create Login.jsx

---
+++ frontend/web_tech/src/Components/Login.jsx
@@ -0,0 +1,65 @@
+// src/Components/Login.js
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import '../Styles/Login.css'; // Import the CSS file for styling
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('https://we67.onrender.com/api/login', {
+        email,
+        password,
+      });
+      if (response.data.success) {
+        console.log(response.data.data.role)
+        localStorage.setItem('email', email);
+       
+        localStorage.setItem('role', response.data.data.role);
+        localStorage.setItem('isLoggedIn', 'true');
+        window.dispatchEvent(new Event('authChange'));
+        navigate(response.data.data.role === 'superadmin' ? '/super' : '/add');
+      } else {
+        setError('Invalid credentials');
+      }
+    } catch (error) {
+      setError('An error occurred. Please try again.');
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Admin Login</h2>
+      {error && <p className="error-message">{error}</p>}
+      <form onSubmit={handleLogin}>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="login-button">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
